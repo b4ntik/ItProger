ff6361edd7665a7b72a35a7df1d939adcb60d6fc
b4ntik <b4ntik@gmail.com>
2023-05-17 00:07:24 +0500

    new

---
+++ ItProger/sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ItProger
+{
+    class sum
+    {
+
+        public static void Main()
+        {
+            int sum = 0;
+            int ost = 0;
+            int ost2 = 0;
+            for (int i = 0; i < 1001; i++)
+            {
+                ost = (i % 3);
+                ost2 = (i % 5);
+                if (ost == 0 || ost2 == 0)
+                {
+                    sum = sum + i;
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+};
